acea2357f992f0f9590780a81674294e42961643
siri <siriprapagerads@gmail.com>
2026-06-19 14:47:41 +0700

    leading title comtent

--- src/component/content/contentVariants.ts
+++ src/component/content/contentVariants.ts
@@ -6,8 +6,8 @@ export const contentVariants = tv({
          flex flex-col justify-center
          break-words
          max-w-[100vw] min-h-[100vh] h-fit`,
-    title: `md:text-5xl text-4xl pb-6 font-bold`,
-    subtitle: `md:text-3xl text-2xl text-blue-500 dark:text-blue-700 pt-4 font-bold`,
+    title: `md:text-5xl text-4xl font-bold mb-4`,
+    subtitle: `md:text-3xl text-2xl text-blue-500 dark:text-blue-700 mb-4 font-bold`,
     content: `text-[14px]`
   }
 });
